63c136c8695dd6355aa63b0684f7b6242ce5027b
obuchheit <obuchheit45@gmail.com>
2024-11-14 22:14:48 -0600

    django-api-iv

---
+++ week6/day2_django_validators_tests/django-api-iv/school_proj/student_app/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Student
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['name', 'student_email', 'locker_number']
+
+class StudentAllSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
